8222c77538e0ce87ab550e583ad2334384d378cc
likanug <likanug.g@qq.com>
2025-05-17 21:20:08 +0800

    重构项目zero2prod::run以返回一个Server,而无需等待

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,15 @@
-use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
+use actix_web::{App, HttpResponse, HttpServer, web};
+use actix_web::dev::Server;
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", name)
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
-pub async fn run() -> std::io::Result<()>{
-    HttpServer::new(|| {
+pub async fn run() -> std::io::Result<Server>{
+    let server = HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
     })
         .bind("127.0.0.1:8000")?
-        .run()
-        .await
+        .run();
+    Ok(server)
 }
