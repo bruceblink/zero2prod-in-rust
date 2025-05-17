47e500507b733582209b9a7c339b1811c9f7dfbf
likanug <likanug.g@qq.com>
2025-05-17 21:34:06 +0800

    重构和删除无用代码，增加"/health_check"的集成测试代码

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use actix_web::dev::Server;
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
-pub async fn run() -> std::io::Result<Server>{
+pub async fn run() -> Result<Server, std::io::Error>{
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
