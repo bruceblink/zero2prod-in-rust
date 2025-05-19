984f52a8c1bdf2dc49f6885f54bfcbf326510406
likanug <likanug.g@qq.com>
2025-05-19 21:33:28 +0800

    增加/subscriptions 接口的集成测试用例

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -23,3 +23,43 @@ fn spawn_app() -> String {
     let _ = tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
+// 添加subscribe的集成测试
+#[tokio::test]
+async fn subscribe_return_200_for_valid_form_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    // 组装请求体报文
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+    // 断言响应码为200
+    assert_eq!(200, response.status().as_u16())
+}
+#[tokio::test]
+async fn subscribe_return_400_when_data_is_missing() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let test_case = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("","missing both name and email")
+    ];
+    for (invalid_body, error_message) in test_case  {
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+        assert_eq!(400, response.status().as_u16(),
+                   "The API did not fail with 400 Bad Request when the payload was {}.",error_message)
+
+    }
+}
