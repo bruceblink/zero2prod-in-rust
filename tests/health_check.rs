8387603eb4114bf18b61f5d39555759613a2bfbb
likanug <likanug.g@qq.com>
2025-05-24 22:26:27 +0800

    更新数据库查询的测试用例

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -32,7 +32,7 @@ async fn subscribe_return_200_for_valid_form_data() {
 
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_string = configuration.database.connection_string();
-    let _connection = PgConnection::connect(&connection_string)
+    let mut connection = PgConnection::connect(&connection_string)
         .await
         .expect("Failed to connect to Postgres. ");
 
@@ -47,7 +47,14 @@ async fn subscribe_return_200_for_valid_form_data() {
         .await
         .expect("Failed to execute request.");
     // 断言响应码为200
-    assert_eq!(200, response.status().as_u16())
+    assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("select email, name from subscriptions",).fetch_one(&mut  connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 #[tokio::test]
 async fn subscribe_return_400_when_data_is_missing() {
