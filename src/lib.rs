16c8566bf51096530beead63da44931a84248ccc
likanug <likanug.g@qq.com>
2025-05-25 15:15:33 +0800

    拆分lib.rs中的函数到subscriptions.rs和health_check.rs中，增加数据库insert的代码，以及更新相关的测试用例

--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,3 @@ pub mod configuration;
 pub mod routes;
 pub mod startup;
 
-use std::net::TcpListener;
-use actix_web::{App, HttpResponse, HttpServer, web};
-use actix_web::dev::Server;
-
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-// 定义表单数据结构体
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String,
-}
-
-// 总是返回200
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
--- src/startup.rs
+++ src/startup.rs
@@ -1,20 +1,20 @@
+use std::net::TcpListener;
 use actix_web::{App, HttpServer, web};
 use actix_web::dev::Server;
-use sqlx::PgConnection;
-use std::net::TcpListener;
+use sqlx::PgPool;
 
-use crate::{health_check, subscribe};
+use crate::routes::{health_check, subscribe};
 
 pub fn run(listener: TcpListener,
-           connection: PgConnection) -> Result<Server, std::io::Error>{
+           db_pool: PgPool) -> Result<Server, std::io::Error>{
     // 智能指针包装一个连接
-    let connection = web::Data::new(connection);
+    let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
           .route("/health_check", web::get().to(health_check))
           .route("/subscriptions", web::post().to(subscribe))
           // 获取连接的副本绑定到应用程序
-          .app_data(connection.clone())
+          .app_data(db_pool.clone())
         })
         .listen(listener)?
         .run();
