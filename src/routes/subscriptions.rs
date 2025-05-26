b4b04046517f65e2b03cda3514bf0c77d6e408f1
likanug <likanug.g@qq.com>
2025-05-26 20:24:50 +0800

    再/subscriptions 路由中添加日志输出

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -11,6 +11,10 @@ pub struct FormData {
 
 pub async fn subscribe(form: web::Form<FormData>,
                        pool: web::Data<PgPool>) -> HttpResponse {
+    log::info!(
+        "Adding '{}' '{}' as a new subscriber.", form.email, form.name
+    );
+    log::info!("Saving new subscriber detail in the database");
     match sqlx::query!(r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         values ($1, $2, $3, $4)"#,
@@ -20,9 +24,12 @@ pub async fn subscribe(form: web::Form<FormData>,
         Utc::now()
     ).execute(pool.get_ref()).await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!("New subscriber details in the database");
+            HttpResponse::Ok().finish()
+        },
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            log::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
