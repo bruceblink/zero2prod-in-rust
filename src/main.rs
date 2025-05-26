b5243a5b92c2ad502e96e10ec391376c496f6def
likanug <likanug.g@qq.com>
2025-05-26 19:48:27 +0800

    添加evn_logger日志组件,初始化日志输出到控制台

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::net::TcpListener;
+use env_logger::Env;
 
 use sqlx::PgPool;
 
@@ -7,6 +8,7 @@ use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()>{
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await.expect("Failed to connect to Postgres.");
