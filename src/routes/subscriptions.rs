f7cc31f3abc866791d25c8bc051f82e779519432
likanug <likanug.g@qq.com>
2025-05-28 21:35:38 +0800

    使用tracing的Instrument插桩future

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,7 @@ use actix_web::{HttpResponse, web};
 use sqlx::PgPool;
 use chrono::Utc;
 use uuid::Uuid;
+use tracing::Instrument;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -12,25 +13,31 @@ pub struct FormData {
 pub async fn subscribe(form: web::Form<FormData>,
                        pool: web::Data<PgPool>) -> HttpResponse {
     let request_id = Uuid::new_v4();
-    tracing::info!(
-        "request_id {} - Adding '{}' '{}' as a new subscriber.", request_id, form.email, form.name
+    // 创建info级别的跨度
+    let request_span = tracing::info_span!(
+        "Add a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
     );
-    tracing::info!("request_id {} - Saving new subscriber detail in the database", request_id);
+    let _request_span_guard = request_span.enter();
+    let query_span = tracing::info_span!("Saving new subscriber detail in the database");
     match sqlx::query!(r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         values ($1, $2, $3, $4)"#,
         Uuid::new_v4(),
         form.email,
         form.name,
-        Utc::now()
-    ).execute(pool.get_ref()).await
+        Utc::now())
+        .execute(pool.get_ref())
+        .instrument(query_span)
+        .await
     {
         Ok(_) => {
-            tracing::info!("request_id {} - New subscriber details in the database", request_id);
             HttpResponse::Ok().finish()
         },
         Err(e) => {
-            tracing::error!("request_id {} - Failed to execute query: {:?}", request_id, e);
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
