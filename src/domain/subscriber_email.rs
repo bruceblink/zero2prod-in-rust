48a2bc882b937008cd4884dd7481a30022b4898d
likanug <likanug.g@qq.com>
2025-06-04 22:25:23 +0800

    使用validator的ValidateEmail进行email的校验,以及增加单元测试

---
+++ src/domain/subscriber_email.rs
@@ -0,0 +1,49 @@
+use validator::ValidateEmail;
+#[derive(Debug)]
+pub struct SubscriberEmail(String);
+
+
+impl SubscriberEmail {
+    pub fn parser(s: String) -> Result<SubscriberEmail, String> {
+        if ValidateEmail::validate_email(&s) {
+            Ok(Self(s))
+        }else {
+            Err(format!("{} is not a valid subscriber email.", s))
+        }
+    }
+}
+
+impl AsRef<str> for SubscriberEmail {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+mod tests {
+    use super::SubscriberEmail;
+    use claim::{assert_err, assert_ok};
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let email = "".to_string();
+        assert_err!(SubscriberEmail::parser(email));
+    }
+
+    #[test]
+    fn email_missing_at_symbol_is_rejected() {
+        let email = "ursuladomain".to_string();
+        assert_err!(SubscriberEmail::parser(email));
+    }
+
+    #[test]
+    fn email_missing_subject_is_rejected() {
+        let email = "@domain.com".to_string();
+        assert_err!(SubscriberEmail::parser(email));
+    }
+
+    #[test]
+    fn email_correct() {
+        let email = "123@gmail.com".to_string();
+        assert_ok!(SubscriberEmail::parser(email));
+    }
+}
