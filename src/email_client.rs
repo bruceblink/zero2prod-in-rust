d1578b85bb8c335762e702bec4f99b5d759ae8f3
likanug <likanug.g@qq.com>
2025-06-10 22:17:01 +0800

    添加email邮件发送组件

---
+++ src/email_client.rs
@@ -0,0 +1,18 @@
+//邮件发送组件
+
+use crate::domain::SubscriberEmail;
+pub struct EmailClient {
+    sender: SubscriberEmail
+}
+
+impl EmailClient {
+    pub async fn send_email(
+        &self,
+        recipient: SubscriberEmail,
+        subject: &str,
+        html_content: &str,
+        text_content: &str
+    )  -> Result<(), String>{
+        todo!()
+    }
+}
