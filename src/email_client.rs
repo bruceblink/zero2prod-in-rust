6a55c9933051eef0ce11f5d00f4d608a3111c0f8
likanug <likanug.g@qq.com>
2025-06-11 20:13:54 +0800

    增加EmailClient的构造方法

--- src/email_client.rs
+++ src/email_client.rs
@@ -1,11 +1,24 @@
 //邮件发送组件
 
+use reqwest::Client;
 use crate::domain::SubscriberEmail;
 pub struct EmailClient {
-    sender: SubscriberEmail
+    http_client: Client,  // 存储Client实例
+    base_url: String,     // 用于存储发送API请求的URL
+    sender: SubscriberEmail,
 }
 
 impl EmailClient {
+
+    // 增加EmailClient的构造方法
+    pub fn new(base_url: String, sender: SubscriberEmail) -> Self {
+        Self {
+            http_client: Client::new(),
+            base_url,
+            sender
+        }
+    }
+
     pub async fn send_email(
         &self,
         recipient: SubscriberEmail,
