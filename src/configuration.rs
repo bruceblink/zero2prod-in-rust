dd65ac751e4ceee99df4e1589e8888efdc09d90b
likanug <likanug.g@qq.com>
2025-06-19 22:56:29 +0800

    删除无用的引用

--- src/configuration.rs
+++ src/configuration.rs
@@ -2,7 +2,6 @@ use secrecy::{ExposeSecret, Secret};
 use serde_aux::field_attributes::deserialize_number_from_string;
 use sqlx::postgres::{PgConnectOptions, PgSslMode};
 use crate::domain::SubscriberEmail;
-use crate::email_client::EmailClient;
 
 #[derive(serde::Deserialize)]
 pub struct Setting {
